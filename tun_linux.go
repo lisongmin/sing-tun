c5090f78151dcd66ccde693db7b645cbc4092233
Songmin Li <lisongmin@protonmail.com>
2023-10-04 17:35:25 +0800

    Fix tun[tun-in]: configure tun interface: file exists

--- tun_linux.go
+++ tun_linux.go
@@ -232,7 +232,10 @@ func (t *NativeTun) configure(tunLink netlink.Link) error {
 			addr4, _ := netlink.ParseAddr(address.String())
 			err = netlink.AddrAdd(tunLink, addr4)
 			if err != nil {
-				return err
+				if err == unix.EEXIST {
+					continue
+				}
+				return E.Cause(err, "add addr v4")
 			}
 		}
 	}
@@ -241,7 +244,10 @@ func (t *NativeTun) configure(tunLink netlink.Link) error {
 			addr6, _ := netlink.ParseAddr(address.String())
 			err = netlink.AddrAdd(tunLink, addr6)
 			if err != nil {
-				return err
+				if err == unix.EEXIST {
+					continue
+				}
+				return E.Cause(err, "add addr v6")
 			}
 		}
 	}
@@ -288,7 +294,7 @@ func (t *NativeTun) configure(tunLink netlink.Link) error {
 
 	err = netlink.LinkSetUp(tunLink)
 	if err != nil {
-		return err
+		return E.Cause(err, "link up")
 	}
 
 	if t.options.TableIndex == 0 {
@@ -717,6 +723,9 @@ func (t *NativeTun) setRoute(tunLink netlink.Link) error {
 	for i, route := range routes {
 		err := netlink.RouteAdd(&route)
 		if err != nil {
+			if err == unix.EEXIST {
+				continue
+			}
 			return E.Cause(err, "add route ", i)
 		}
 	}
@@ -727,6 +736,9 @@ func (t *NativeTun) setRules() error {
 	for i, rule := range t.rules() {
 		err := netlink.RuleAdd(rule)
 		if err != nil {
+			if err == unix.EEXIST {
+				continue
+			}
 			return E.Cause(err, "add rule ", i, "/", len(t.rules()))
 		}
 	}
